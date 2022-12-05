a24a7aada8c0b3415502ad76220b48fc6da87450
yamaan93 <yamaan93@gmail.com>
2022-12-05 07:50:52 -0500

    shift text

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,17 @@
 #include <Arduino.h>
-#include <SPI.h> 
-#include <mcp2515.h> 
+#include <SPI.h>
+#include <mcp2515.h>
 
 struct can_frame canMsg;
 
-MCP2515 mcp2515(10); //spi pin is 10
+MCP2515 mcp2515(10); // spi pin is 10
 
-
-
-void setup() {
+void setup()
+{
   Serial.begin(9600); // begin Serial communication at 9600 buadrate
-  SPI.begin(); //begin SPI Communication
+  SPI.begin();        // begin SPI Communication
   mcp2515.reset();
-  mcp2515.setBitrate(CAN_500KBPS,MCP_8MHZ); //CAN Speed at 500KBPS and 8MHz crystal
+  mcp2515.setBitrate(CAN_500KBPS, MCP_8MHZ); // CAN Speed at 500KBPS and 8MHz crystal
   mcp2515.setNormalMode();
 
   delay(3000);
@@ -20,21 +19,22 @@ void setup() {
   Serial.print("Setup Complete \n");
 }
 
-void loop() {
-  if (mcp2515.readMessage(&canMsg) == MCP2515::ERROR_OK) {
-        
-        //expecting the first 2 data points to be speed and temp
-        int speed = canMsg.data[0];
-        int temp = canMsg.data[1];
+void loop()
+{
+  Serial.println(mcp2515.readMessage(&canMsg));
+  if (mcp2515.readMessage(&canMsg) == MCP2515::ERROR_OK)
+  {
+    Serial.print(canMsg.can_id, HEX); // print ID
+    Serial.print(" ");
+    Serial.print(canMsg.can_dlc, HEX); // print DLC
+    Serial.print(" ");
 
-        char message[80];
-
-        sprintf (message, "Temperature: %d Speed: %d",temp, speed);
-
-        Serial.print(message);
-
+    for (int i = 0; i < canMsg.can_dlc; i++)
+    { // print the data
+      Serial.print(canMsg.data[i], HEX);
+      Serial.print(" ");
     }
 
-
-
+    Serial.println();
+  }
 }
